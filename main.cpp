51a20ce56d956f8aa07f11899ee20226d5bb5ea4
Phil Jonas <phil.jonas@saltus.co.uk>
2025-03-21 23:05:10 +0000

    constraint

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <SFML/Graphics.hpp>
 
+sf::Vector2f normalise_vector(float w, float h, float dist)
+{
+    return sf::Vector2f(w/dist, h/dist);
+}
+
+void circle_constraint(sf::CircleShape &anchor, sf::CircleShape &target, float constraint)
+{
+    float width = anchor.getPosition().x - target.getPosition().x;
+    float height = anchor.getPosition().y - target.getPosition().y;
+    float distance = std::sqrt(width * width + height * height);
+    if(distance < constraint){
+        return;
+    }
+
+    // get unit vector
+    sf::Vector2f unit = normalise_vector(width, height, distance);
+
+    // multiply unit vector by constraint
+    sf::Vector2f projection = sf::Vector2f(unit.x * constraint, unit.y * constraint) + target.getPosition();
+
+    // constrain follower
+    target.setPosition(projection);
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Circle Constraints");
@@ -35,9 +59,9 @@ int main()
             shape.move(0.f, 0.1f);
         }
 
+        circle_constraint(shape, follower, 100.f);
 
         // DRAW
-
         window.clear();
         window.draw(follower);
         window.draw(shape);
