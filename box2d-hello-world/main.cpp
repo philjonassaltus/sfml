853263f9ea260776a698c1157651d97b6efe5cad
Phil Jonas <phil.jonas@saltus.co.uk>
2025-03-22 14:53:58 +0000

    works now

--- box2d-hello-world/main.cpp
+++ box2d-hello-world/main.cpp
@@ -2,6 +2,9 @@
 #include <box2d/box2d.h>
 #include <iostream>
 
+// Scale factor for Box2D to SFML conversion
+const float SCALE = 30.0f;
+
 int main() {
     // Create SFML window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML & Box2D");
@@ -16,36 +19,47 @@ int main() {
     // Create text
     sf::Text text("Hello, SFML & Box2D!", font, 30);
     text.setFillColor(sf::Color::White);
-    text.setPosition(200, 250);
+    text.setPosition(200, 50);
 
     // Set up Box2D world
     b2WorldDef worldDef = b2DefaultWorldDef();
-    worldDef.gravity = (b2Vec2){0.0f, -10.0f};
+    worldDef.gravity = (b2Vec2){0.0f, 10.0f};
     b2WorldId worldId = b2CreateWorld(&worldDef);
 
     // Create a Box2D ground body
     b2BodyDef groundBodyDef = b2DefaultBodyDef();
-    groundBodyDef.position = (b2Vec2){0.0f, -10.0f};
+    groundBodyDef.position = (b2Vec2){400.0f / SCALE, 550.0f / SCALE};  // Centered in window
     b2BodyId groundId = b2CreateBody(worldId, &groundBodyDef);
-    
-    b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
+
+    b2Polygon groundBox = b2MakeBox(400.0f / SCALE, 10.0f / SCALE);
     b2ShapeDef groundShapeDef = b2DefaultShapeDef();
     b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
+
+    // Create an SFML rectangle for the ground
+    sf::RectangleShape groundRect(sf::Vector2f(800, 20));
+    groundRect.setFillColor(sf::Color::Green);
+    groundRect.setOrigin(400, 10);
+    groundRect.setPosition(400, 550);
 
     // Create a dynamic body
     b2BodyDef bodyDef = b2DefaultBodyDef();
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position = (b2Vec2){0.0f, 4.0f};
+    bodyDef.position = (b2Vec2){400.0f / SCALE, 200.0f / SCALE};
     b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
 
-    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
+    b2Polygon dynamicBox = b2MakeBox(30.0f / SCALE, 30.0f / SCALE);
     b2ShapeDef shapeDef = b2DefaultShapeDef();
     shapeDef.density = 1.0f;
     shapeDef.friction = 0.3f;
     b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
 
-    int subStepCount = 4;
+    // Create an SFML rectangle for the Box2D dynamic body
+    sf::RectangleShape box(sf::Vector2f(60, 60));
+    box.setFillColor(sf::Color::Blue);
+    box.setOrigin(30, 30);
 
+    // Physics step settings
+    int subStepCount = 4;
     sf::Clock clock;
 
     // Main loop
@@ -59,13 +73,20 @@ int main() {
         // Box2D physics step
         float timeStep = clock.restart().asSeconds();
         b2World_Step(worldId, timeStep, subStepCount);
+
+        // Get body position and rotation
         b2Vec2 position = b2Body_GetPosition(bodyId);
         b2Rot rotation = b2Body_GetRotation(bodyId);
-        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, b2Rot_GetAngle(rotation));
 
-        // Update SFML scene
+        // Update SFML rectangle position & rotation
+        box.setPosition(position.x * SCALE, position.y * SCALE);
+        box.setRotation(b2Rot_GetAngle(rotation) * 180.0f / b2_pi);
+
+        // Render scene
         window.clear();
         window.draw(text);
+        window.draw(groundRect);
+        window.draw(box);
         window.display();
     }
 
